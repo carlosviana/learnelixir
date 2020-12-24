0ac9330c6c62c3661ca64571e874926bb5d47bbd
carlosviana <viana.mail@gmail.com>
2020-12-24 12:38:18 -0300

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,40 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, DiscussWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "SojArDQ+2qSb6k7O/nPqNRfVNXAMlWaktiq8vMdiSISbGN/2gz2PqCJeZRFUCDkT",
+  render_errors: [view: DiscussWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Discuss.PubSub,
+  live_view: [signing_salt: "Jvp9HqyG"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "7ac0a130cbd6c00a1446",
+  client_secret: "3b93eb42c7824d376bb4a8847a9f763f3a83f29e"
---
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -0,0 +1,48 @@
+defmodule DiscussWeb.AuthController do
+  use DiscussWeb, :controller
+  plug Ueberauth
+
+  alias Discuss.User
+  alias Discuss.Repo
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    # IO.puts "+++++"
+    # IO.inspect(conn.assigns)
+    # IO.puts "+++++"
+    # IO.inspect(params)
+    # IO.puts "+++++"
+
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+
+    changeset = User.changeset(%User{}, user_params)
+
+    sigin(conn, changeset)
+
+  end
+
+  defp sigin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user } ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, _reason } ->
+        conn
+        |> put_flash(:error, "Error singing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+
+  end
+
+end
